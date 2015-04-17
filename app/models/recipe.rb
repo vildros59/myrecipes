738b3eb567a55b260d5d58a7a1b00d5c7ccc4f86
vildros59 <vildros59@yahoo.se>
2015-04-17 11:42:06 +0000

    Create chefs and recipes association and tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
-class Recipe < ActiveRecord::Base
-  
+class Recipe < ActiveRecord::Base 
+  belongs_to :chef    # Create association    
+  validates :chef_id, presence: true     
+  # The test should validate the name and enforce that it has to be present. Otherwise the recipe should not be saved in database!  
+  validates :name, presence: true, length: { minimum: 5, maximum: 100 }        
+  validates :summary, presence: true, length: { minimum: 10, maximum: 150 }   
+  validates :description, presence: true, length: { minimum: 20, maximum: 500 }      
 end    
